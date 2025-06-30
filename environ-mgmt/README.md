5289debe03f508ad66ce509334e98774b667535e
Thomas Naughton <naughtont@ornl.gov>
2025-06-30 15:38:05 -0400

    envmgmt: error class/code/string test
    Tests for MPI_Remove_error_{class,code,string} added in MPI 4.1
    
    Notes:
    - autotooling to dynamically check for MPI_Remove_error_* fns
      Note: auto-tooling is mostly copy/paste from Jeff's status test
    
    - if symbols not found, skips.  However, if we detect we
      have a new enough MPI >= 4.1, we will print msg that these should
      exist.
    
    Signed-off-by: Thomas Naughton <naughtont@ornl.gov>

---
+++ environ-mgmt/README.md
@@ -0,0 +1,63 @@
+Environ Management Tests
+------------------------
+
+Tests for items in the "MPI Environmental Management" chapter.
+
+These are begin created in the ompi-tests-public repository
+to ensure they are easy to access.
+
+Using autotools to detect if we have the MPI-4.1 remove error code
+functionality, otherwise skips.
+
+Usage
+-----
+ - Ensure MPI `mpicc` in `PATH`
+ - Run autogen/configure/make to build test
+
+   ```
+     cd environ-mgmt/
+     ./autogen.sh && ./configure && make
+   ```
+
+ - Run test(s)
+
+   ```
+     mpirun -np 1 ./src/test_add_del_err_codes
+   ```
+
+Example Output
+--------------
+
+ - On MPI 3.x
+    ```
+    laptop:$ mpirun -np 1 ./src/test_add_del_err_codes
+    TEST: Success creating error class/code/string (last=92, newlast=94)
+    ==== Adds done ====
+    ==== Remove estring ====
+    Warning: MPI_Remove_error_xxx code/class/string NOT available!
+    ==== Remove ecode ====
+    Warning: MPI_Remove_error_xxx code/class/string NOT available!
+    ==== Remove eclass  ====
+    Warning: MPI_Remove_error_xxx code/class/string NOT available!
+    DONE: Success
+    laptop$ echo $?
+    0
+    laptop$
+    ```
+
+ - On MPI 4.1 or later
+    ```
+    laptop:$ mpirun -np 1 ./src/test_add_del_err_codes
+    TEST: Success creating error class/code/string (last=92, newlast=94)
+    ==== Adds done ====
+    ==== Remove estring ====
+    ==== Remove ecode ====
+    TEST: Success reomved error code (oldlast=94, newlast=93)
+    ==== Remove eclass  ====
+    TEST: Success reomved error class (oldlast=93, newlast=92)
+    DONE: Success
+    laptop$ echo $?
+    0
+    laptop$
+    ```
+
---
+++ environ-mgmt/src/test_add_del_err_codes.c
@@ -0,0 +1,212 @@
+/*
+ * Copyright (c) 2025      UT-Battelle, LLC.    All rights reserved.
+ *
+ * Tests for MPI_LASTUSEDCODE, MPI_Add_error_{class,code,string}, and
+ * MPI_Remove_error{class,code,string}.
+ *
+ * File: test_add_del_err_codes.c
+ *
+ * Referenced examples online and in ompi-tests:
+ *  - "ompi-tests/random/add_error_class.c"
+ *  - example from Lisandro Dalcin
+ *    https://www.open-mpi.org/community/lists/devel/2014/04/14578.php
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mpi.h"
+
+#define SUCCESS  (0)
+#define FAILURE  (-1)
+#define SKIPTEST (-2)
+
+#define CHECK_RC(_rc,_msg) do {\
+    if (_rc != MPI_SUCCESS) { \
+        fprintf(stderr, "[%s:%d] Error: %s failed (rc=%d)\n", __FUNCTION__, __LINE__, _msg, _rc); \
+        exit(1); \
+    } \
+   } while(0)
+
+int get_lastused(MPI_Comm comm);
+int do_add_error(MPI_Comm comm, int *eclass, int *ecode, char *estr);
+int do_del_error_string(MPI_Comm comm, int eclass, int ecode, char *estr);
+int do_del_error_code(MPI_Comm comm, int eclass, int ecode, char *estr);
+int do_del_error_class(MPI_Comm comm, int eclass, int ecode, char *estr);
+
+int get_lastused(MPI_Comm comm)
+{
+    int *last = NULL;
+    int flag = 0;
+
+    CHECK_RC( MPI_Comm_get_attr(comm, MPI_LASTUSEDCODE, &last, &flag),
+              "MPI_Comm_get_attr");
+
+    if (flag == 0)
+        return (-1);   /* no key found */
+    else
+        return (*last);
+}
+
+int do_add_error(MPI_Comm comm, int *eclass, int *ecode, char *estr)
+{
+    int ret = SUCCESS;
+
+#ifdef HAVE_MPI_ADD_ERROR_CODE
+    int last = 0, newlast = 0;
+    last = get_lastused(MPI_COMM_WORLD);
+
+    CHECK_RC( MPI_Add_error_class(eclass),        "MPI_Add_error_class");
+    CHECK_RC( MPI_Add_error_code(*eclass, ecode), "MPI_Add_error_code");
+    CHECK_RC( MPI_Add_error_string(*ecode, estr), "MPI_Add_error_string");
+
+    newlast = get_lastused(MPI_COMM_WORLD);
+
+    if (newlast > last) {
+        printf("TEST: Success creating error class/code/string (last=%d, newlast=%d)\n", last, newlast);
+        ret = SUCCESS;
+    } else {
+        printf("Error: MPI_Add_error_xxx failed LastUsedCode not increased (last=%d, new=%d)\n", last, newlast);
+        ret = FAILURE;
+    }
+
+#else
+    printf("Warning: MPI_Add_error_xxx code/class/string NOT available!\n");
+    ret = SKIPTEST;
+#endif /* HAVE_MPI_ADD_ERROR_CODE */
+
+    return(ret);
+}
+
+
+int do_del_error_string(MPI_Comm comm, int eclass, int ecode, char *estr)
+{
+    int ret = SUCCESS;
+
+#ifdef HAVE_MPI_REMOVE_ERROR_CODE
+    CHECK_RC( MPI_Remove_error_string(ecode), "MPI_Remove_error_string");
+#else
+    /* We should have MPI_Remove_error_xxx with MPI >= 4.1 */
+    printf("Warning: MPI_Remove_error_xxx code/class/string NOT available!\n");
+    ret = SKIPTEST;
+#endif /* HAVE_MPI_REMOVE_ERROR_CODE */
+
+    return(ret);
+}
+
+int do_del_error_code(MPI_Comm comm, int eclass, int ecode, char *estr)
+{
+    int ret = SUCCESS;
+
+#ifdef HAVE_MPI_REMOVE_ERROR_CODE
+    int last = 0, newlast = 0;
+    last = get_lastused(MPI_COMM_WORLD);
+
+    CHECK_RC( MPI_Remove_error_code(ecode),   "MPI_Remove_error_code");
+
+    newlast = get_lastused(MPI_COMM_WORLD);
+
+    if (newlast < last) {
+        printf("TEST: Success reomved error code (oldlast=%d, newlast=%d)\n", last, newlast);
+        ret = SUCCESS;
+    } else {
+        printf("Error: MPI_Remove_error_xxx failed LastUsedCode not decreased (last=%d, new=%d)\n", last, newlast);
+        ret = FAILURE;
+    }
+
+#else
+    /* We should have MPI_Remove_error_xxx with MPI >= 4.1 */
+    printf("Warning: MPI_Remove_error_xxx code/class/string NOT available!\n");
+    ret = SKIPTEST;
+#endif /* HAVE_MPI_REMOVE_ERROR_CODE */
+
+    return(ret);
+}
+
+int do_del_error_class(MPI_Comm comm, int eclass, int ecode, char *estr)
+{
+    int ret = SUCCESS;
+
+#ifdef HAVE_MPI_REMOVE_ERROR_CODE
+    int last = 0, newlast = 0;
+    last = get_lastused(MPI_COMM_WORLD);
+
+    CHECK_RC( MPI_Remove_error_class(eclass), "MPI_Remove_error_class");
+
+    newlast = get_lastused(MPI_COMM_WORLD);
+
+    if (newlast < last) {
+        printf("TEST: Success reomved error class (oldlast=%d, newlast=%d)\n", last, newlast);
+        ret = SUCCESS;
+    } else {
+        printf("Error: MPI_Remove_error_xxx failed LastUsedCode not decreased (last=%d, new=%d)\n", last, newlast);
+        ret = FAILURE;
+    }
+
+#else
+    /* We should have MPI_Remove_error_xxx with MPI >= 4.1 */
+    printf("Warning: MPI_Remove_error_xxx code/class/string NOT available!\n");
+    ret = SKIPTEST;
+#endif /* HAVE_MPI_REMOVE_ERROR_CODE */
+
+    return(ret);
+}
+
+
+int main (int argc, char **argv)
+{
+    int rc, rank=0, size;
+    int retval = EXIT_SUCCESS;
+    int eclass, ecode;
+    char *estr = "My Dummy Error String";
+
+    CHECK_RC( MPI_Init(&argc, &argv),               "MPI_Init");
+    CHECK_RC( MPI_Comm_rank(MPI_COMM_WORLD, &rank), "MPI_Comm_rank");
+    CHECK_RC( MPI_Comm_size(MPI_COMM_WORLD, &size), "MPI_Comm_size");
+
+    rc = do_add_error(MPI_COMM_WORLD, &eclass, &ecode, estr);
+    if ((SUCCESS != rc) && (SKIPTEST != rc)) {
+        printf("Error: Failed during do_add_error testing (rc=%d)\n", rc);
+        retval = EXIT_FAILURE;
+        goto fini;
+    }
+
+    printf("==== Adds done ====\n");
+
+    printf("==== Remove estring ====\n");
+    rc = do_del_error_string(MPI_COMM_WORLD, eclass, ecode, estr);
+    if ((SUCCESS != rc) && (SKIPTEST != rc)) {
+        printf("Error: Failed during do_del_error_string testing (rc=%d)\n", rc);
+        retval = EXIT_FAILURE;
+        goto fini;
+    }
+
+    printf("==== Remove ecode ====\n");
+    rc = do_del_error_code(MPI_COMM_WORLD, eclass, ecode, estr);
+    if ((SUCCESS != rc) && (SKIPTEST != rc)) {
+        printf("Error: Failed during do_del_error_code testing (rc=%d)\n", rc);
+        retval = EXIT_FAILURE;
+        goto fini;
+    }
+
+    printf("==== Remove eclass  ====\n");
+    rc = do_del_error_class(MPI_COMM_WORLD, eclass, ecode, estr);
+    if ((SUCCESS != rc) && (SKIPTEST != rc)) {
+        printf("Error: Failed during do_del_error_class testing (rc=%d)\n", rc);
+        retval = EXIT_FAILURE;
+        goto fini;
+    }
+
+fini:
+    CHECK_RC( MPI_Finalize(), "MPI_Finalize");
+
+    if (rank == 0) {
+        if (retval == EXIT_SUCCESS) {
+            printf("DONE: Success\n");
+        } else {
+            printf("DONE: Failure\n");
+        }
+    }
+
+    return(retval);
+}
+
